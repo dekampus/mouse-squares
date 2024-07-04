42df1e381448d818b8e23d67bb8233d2d9b0298f
dekampus <moiseswilliam.cs@gmail.com>
2024-07-04 10:51:13 -0300

    Add fade in when coloring squares

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ function createSquares(numberOfSquaresColumns, numberOfSquaresRows) {
         for (let j = 0; j < numberOfSquaresColumns; j++) {
             let div = document.createElement('div');
             div.classList.add('square');
+            div.style.opacity = 1;
     
             rowContainer.appendChild(div);
             container.appendChild(rowContainer);
         }
     }
+}
+
+function fadeOut(square) {
+    let interval = setInterval(() => {
+        let opacity = square.style.opacity;
+        if (opacity > 0) {
+            opacity -= 0.001;
+            square.style.opacity = opacity;
+            square.style.borderColor = '#ffb0f9';
+        } else {
+            clearInterval(interval);
+            opacity = 1;
+            square.style.opacity = opacity;
+            square.style.backgroundColor = 'white';
+            square.style.borderColor = '#ffb0f9';
+        }
+    }, 1);
 }
 
 createSquares(numberOfSquaresColumns, numberOfSquaresRows);
@@ -25,5 +43,7 @@ for (let square of squares) {
     square.addEventListener('mouseenter', (e) => {
         e.target.style.backgroundColor = '#85317e';
         e.target.style.borderColor = '#85317e';
+
+        fadeOut(square);
     })
 }
